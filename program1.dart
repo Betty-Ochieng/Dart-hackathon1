dea5dcf3bfa3b50158a0080d96b7e0327b9cbb9f
Betty-Ochieng <160406261+Betty-Ochieng@users.noreply.github.com>
2024-03-18 16:36:55 +0300

    Add files via upload

---
+++ program1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Betty";
+  int age = 25;
+  String school = "University of Nairobi";
+  String hobby = "Playing piano";
+
+  print("My name is $name. I am $age years old, studying at $school, and my hobby is $hobby.");
+}
